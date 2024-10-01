cb8cf5fee135ceff18bb2f1bbf7f4abf9cc55900
Augusto <gutoorregoo@gmail.com>
2024-10-01 16:58:26 -0300

    primeiro commit

---
+++ src/scripts/services/getUser.js
@@ -0,0 +1,8 @@
+import { baseUrl } from "/src/scripts/variables.js"
+
+const getUser = async (userName) => {
+    const response = await fetch(`${baseUrl}/${userName}`)
+    return await response.json()
+}
+
+export { getUser }
